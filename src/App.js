46a63b242c559c78b302dabae460290c33f21fc2
Tonny654 <lambal.tony@outlook.fr>
2025-06-20 16:00:00 +0200

    Création du composant Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './composants/Nav-bar.jsx';
+import Footer from './composants/Footer.jsx'; 
 
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/composants/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <Footer style="text-align:center; padding:1rem; font-size:.875rem;">© 2025 FocusBoost Tonny</Footer>
+        </div>
+    );
+};
+
+export default Footer;
