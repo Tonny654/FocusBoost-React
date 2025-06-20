9e451dec9385fb2653a77c89a9239a70032c67fa
Tonny654 <lambal.tony@outlook.fr>
2025-06-20 15:42:04 +0200

    navbar importé

--- src/composants/Nav-bar.jsx
+++ src/composants/Nav-bar.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            
+             <header>
+          <h2 className="title">🚀 FocusBoost</h2>
+          <button className="menu-toggle" id="menu-btn">☰</button>
+          <nav id="menu">
+          <a href="./index.html">Accueil</a>
+          <a href="./PG/Timer/index.html">Timer</a>
+          <a href="./PG/Dashboard/index.html">Tableau de bord</a>
+          </nav>
+        </header>
         </div>
     );
 };
